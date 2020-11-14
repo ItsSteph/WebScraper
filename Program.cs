7023d279cbcdb14ee7e64ef4aceeaeac84fa0bdc
ItsSteph <steph.tronier.vip@gmail.com>
2020-11-14 13:37:22 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractHref("https://www.dr.dk/");
+
+        }
+
+        static void ExtractHref(string URL)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc = web.Load(URL);
+
+            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                HtmlAttribute att = link.Attributes["href"];
+                if (att.Value.Contains("a"))
+                {
+                    Console.WriteLine(att.Value);
+                }
+            }
+        }
+    }
+}
